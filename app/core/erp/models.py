db5f3bc84d91821201ff16ae2115b7b85020e704
Aguilar0607 <emmanuelaguilar0607@gmail.com>
2022-12-12 20:16:21 -0600

    Models Django

--- app/core/erp/models.py
+++ app/core/erp/models.py
@@ -1,9 +1,35 @@
 from django.db import models
 from datetime import datetime
 # Create your models here.
+
+class Type(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        ordering = ['id']
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['id']
+
+
 class Employee(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Nombres')
-    CURP = models.CharField(max_length=18, unique=True, verbose_name='CURP')
+    categ = models.ManyToManyField(Category)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    names = models.CharField(max_length=150, verbose_name='Nombres')
+    curp = models.CharField(max_length=18, unique=True, verbose_name='CURP')
     date_joined = models.DateField(default=datetime.now, verbose_name='Fecha de registro')
     date_creation = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now_add=True)
@@ -19,5 +45,4 @@ class Employee(models.Model):
     class Meta:
         verbose_name = 'Empleado'
         verbose_name_plural = 'Empleados'
-        db_table = 'empleado'
         ordering = ['id']
